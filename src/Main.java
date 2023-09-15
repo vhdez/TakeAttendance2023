b3034bbecf9003ca1e5eb1f413c2a2960dc9b773
Noah Frank <64226459+noahf2020@users.noreply.github.com>
2023-09-15 10:01:56 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Students, please tell me that you are present:");
-
-
-
-
-
         System.out.println("Ezra, says: I am here");
+        System.out.println("Noah, says: I am here");
+
     }
 }
