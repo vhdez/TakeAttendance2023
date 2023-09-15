bc23ac93d333aae47f9bf4a77240a643a4777071
brandonkay <bkay24@scienceleadership.org>
2023-09-15 10:08:03 -0400

    I am here

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,15 @@ public class Main {
 
         System.out.println("Students, please tell me that you are present:");
         System.out.println("Gia Jackson,  says: I'm here.");
-        System.out.println("Ezra, says: I am here");
-        System.out.println("Noah, says: I am here");
+        System.out.println("Gia, says: I'm here.");
+        System.out.println("Noah, says: I'm here");
+
+
         System.out.println("Liam, says: I am here");
         System.out.println("Keith, says: I am here");
 
+
+
+        System.out.println("Brandon, says: I am here.");
     }
 }
