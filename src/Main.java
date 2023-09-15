332a75de3ea775515e2f5bcab8e0b83030eb1e80
angeloskoroli <akoroli25@scienceleadership.org>
2023-09-15 10:00:07 -0400

    Angelos:I am here

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Students, please tell me that you are present:");
-        System.out.println("Angelos: I am here.");
+        System.out.println("Angelos: I am here");
     }
 }
