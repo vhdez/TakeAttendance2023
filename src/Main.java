1124cc5014cfdb91deb060953b7e3e9de7af152a
ezragoldenberg <egoldenberg25@scienceleadership.org>
2023-09-15 09:57:51 -0400

    Ezra is here

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Students, please tell me that you are present:");
+
+
+
+
+
+        System.out.println("Ezra, says: I am here");
     }
 }
