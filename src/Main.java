12b1c189e85df4c3d6b8e897b795063fd20a721f
vhdez <vhernandez@scienceleadership.org>
2023-09-15 09:56:36 -0400

    Re-add Gia

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Students, please tell me that you are present:");
+        System.out.println("Gia, says: I'm here.");
+
     }
 }
